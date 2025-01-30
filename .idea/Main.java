e2c38c8e6dd3bb613792aa3b06ecb305d35ec6c3
its-Simy <135281067+its-Simy@users.noreply.github.com>
2025-01-30 17:56:29 -0500

    First commit
    Just created the main and linked list classes and files in order to set up everything. No real implementation yet.

---
+++ .idea/Main.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+
+    /**The following code will be the basic testing to see if priority is working
+
+     Should add 3 to 5 elements in order to test priority
+
+     */
+
+
+
+    
+}
